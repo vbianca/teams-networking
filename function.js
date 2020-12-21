6237cd9e03520d6038599736b08878457ebdf666
vbianca <bianca.nimigean@yahoo.com>
2020-12-21 21:12:11 +0200

    simple search

--- function.js
+++ function.js
@@ -1,5 +1,3 @@
-console.log('test script');
-
 function insertPersons (persons) {
     const tbody = document.querySelector('#list tbody');
    
@@ -17,9 +15,31 @@ function getPersonHTML (person) {
     <td><a target="_blank" href="https://github.com/${gitHub}">GitHub</a></td>
  </tr>`;
 }
+let allPersons = [];
 
 fetch('team.json')
   .then(res => res.json())
   .then(data =>{
+      allPersons = data;
       insertPersons(data);
+
   });
+
+function searchPersons(text){
+    console.warn("search", text, allPersons);
+    return allPersons.filter(person =>{
+        console.info(person.firstName);
+        return person.firstName == text;
+    });
+
+    return [];
+}
+
+  const search = document.getElementById('search');
+  search.addEventListener("input", e => {
+      const text = e.target.value;
+      
+      const filtrate =searchPersons(text);
+
+      insertPersons(filtrate);
+  })
