7787a1726a107814150d719d2d990022714ccaa2
vbianca <bianca.nimigean@yahoo.com>
2021-01-11 16:08:54 +0200

    add team member

--- function.js
+++ function.js
@@ -1,3 +1,24 @@
+const API = {
+    CREATE: {
+        URL:"create.json",
+        METHOD:"GET" //POST
+    },
+    READ: {
+        URL:"team.json",
+        METHOD:"GET"
+    },
+    UPDATE: {
+        URL:"",
+        METHOD:"GET"
+    },
+    DELETE: {
+        URL:"",
+        METHOD:"GET"
+    }
+};
+
+API.READ.URL
+
 function insertPersons (persons) {
     const tbody = document.querySelector('#list tbody');
    
@@ -13,17 +34,22 @@ function getPersonHTML (person) {
     <td>${person.firstName}</td>
     <td>${person.lastName}</td>
     <td><a target="_blank" href="https://github.com/${gitHub}">GitHub</a></td>
+     <td></td>
  </tr>`;
 }
 let allPersons = [];
 
-fetch('team.json')
-  .then(res => res.json())
-  .then(data =>{
-      allPersons = data;
-      insertPersons(data);
+function loadList(){
+    fetch(API.READ.URL)
+    .then(res => res.json())
+    .then(data =>{
+        allPersons = data;
+        insertPersons(data);
+   });
+}
 
-  });
+loadList();
+
 
 function searchPersons(text){
     text = text.toLowerCase();
@@ -43,4 +69,39 @@ function searchPersons(text){
       const filtrate =searchPersons(text);
 
       insertPersons(filtrate);
-  })
+  });
+
+function saveTeamMember() {
+
+    const firstName = document.querySelector("#list input[name=firstName]").value;
+    const lastName = document.querySelector("input[name=lastName]").value;
+    const gitHub = document.querySelector("input[name=gitHub]").value;
+                  
+    const person = {
+        firstName,
+         lastName, 
+         gitHub: gitHub
+    };
+    console.info('saving...', person, JSON.stringify(person));
+     
+
+
+    fetch(API.CREATE.URL, {
+       method: API.CREATE.METHOD,
+       body: API.CREATE.METHOD === "GET" ? null: JSON.stringify(person)
+    })
+         .then(res => res.json())
+         .then(r => {
+             console.warn(r);
+             if (r.success){
+                 alert('saving data...., please wait until we are ready.')
+                    console.info('refresh list');
+                    loadList();
+             } 
+         });
+}
+
+  const saveBtn = document.querySelector("#list tfoot button ");
+  saveBtn.addEventListener("click", () => {
+      saveTeamMember();
+  });
